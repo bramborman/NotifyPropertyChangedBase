28e63340c71dc97318934a0ab070041f7218a511
Marian Dolinský <mariandolinsky@outlook.com>
2017-03-19 19:10:10 +0100

    Added forgotten things to the Uap project

--- SharedAssemblyInfo.cs
+++ SharedAssemblyInfo.cs
@@ -7,7 +7,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCopyright("© 2017 Marian Dolinský")]
 [assembly: ComVisible(false)]
 // Managed in appveyor.yml
-// Do not comment this
-[assembly: AssemblyVersion("0.1.0.0")]
-[assembly: AssemblyFileVersion("0.1.0.0")]
-[assembly: AssemblyInformationalVersion("0.1.0.0")]
+// [assembly: AssemblyVersion("0.1.0.0")]
+// [assembly: AssemblyFileVersion("0.1.0.0")]
+// [assembly: AssemblyInformationalVersion("0.1.0.0")]
