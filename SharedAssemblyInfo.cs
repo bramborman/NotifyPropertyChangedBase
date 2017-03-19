db319ca512e9b4bda1b509d654f1d559531e8682
Marian Dolinský <mariandolinsky@outlook.com>
2017-03-19 17:46:46 +0100

    Hope this will fix the build error on assembly version patching

--- SharedAssemblyInfo.cs
+++ SharedAssemblyInfo.cs
@@ -7,5 +7,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCopyright("© 2017 Marian Dolinský")]
 [assembly: ComVisible(false)]
 // Managed in appveyor.yml
-// [assembly: AssemblyVersion("0.1.0.0")]
-// [assembly: AssemblyFileVersion("0.1.0.0")]
+// Do not comment this
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
+[assembly: AssemblyInformationalVersion("0.1.0.0")]
