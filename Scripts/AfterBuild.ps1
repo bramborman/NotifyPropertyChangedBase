e2cd38c76e60dff021714e9a77d9b731cede9889
Marian Dolinský <mariandolinsky@outlook.com>
2017-03-21 18:03:12 +0100

    Removed testing messages

--- Scripts/AfterBuild.ps1
+++ Scripts/AfterBuild.ps1
@@ -15,8 +15,5 @@ foreach ($projectFolder in $projectFolders)
 	Push-AppveyorArtifact $zipFileName
 }
 
-Add-AppveyorMessage "TEST - PR number: $env:APPVEYOR_PULL_REQUEST_NUMBER"
-Add-AppveyorMessage "TEST - PR title: $env:APPVEYOR_PULL_REQUEST_TITLE"
-
 NuGet pack -Version $env:APPVEYOR_BUILD_VERSION
 Push-AppveyorArtifact *.nupkg
