7b8cb8566156d74e86fcd16c056a1ba28054b14d
Marian Dolinský <mariandolinsky@outlook.com>
2017-03-21 17:28:34 +0100

    Updated CI related things

--- Scripts/BeforeBuild.ps1
+++ Scripts/BeforeBuild.ps1
@@ -1,11 +1,12 @@
 if ($env:APPVEYOR_REPO_BRANCH -eq "master")
 {
 	$newVersion = $env:APPVEYOR_BUILD_VERSION -replace "-$env:APPVEYOR_REPO_BRANCH"
-	
-	# Write the message using APPVEYOR_BUILD_VERSION variable before we change it using the Update-AppveyorBuild function
-	Add-AppveyorMessage "Build version changed from $env:APPVEYOR_BUILD_VERSION to $newVersion"
+	$message    = "Build version changed from '$env:APPVEYOR_BUILD_VERSION' to '$newVersion'"
 
 	Update-AppveyorBuild -Version $newVersion
+	
+	Add-AppveyorMessage $message
+	Write-Host $message
 }
 
 NuGet restore
