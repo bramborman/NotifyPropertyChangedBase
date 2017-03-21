0ec8cbd36eba7a863210c3f2a89e94fecdae6e60
Marian Dolinský <mariandolinsky@outlook.com>
2017-03-21 18:53:14 +0100

    Changed AppVeyor build version format, disabled MyGet releases until public release, updated nuget versioning to not include branch and build when in master branch, build version is not changed in master branch now, minor changes

--- Scripts/AfterBuild.ps1
+++ Scripts/AfterBuild.ps1
@@ -15,5 +15,18 @@ foreach ($projectFolder in $projectFolders)
 	Push-AppveyorArtifact $zipFileName
 }
 
-NuGet pack -Version $env:APPVEYOR_BUILD_VERSION
+$nugetVersion = $env:APPVEYOR_BUILD_VERSION
+
+if ($env:APPVEYOR_REPO_BRANCH -eq "master")
+{
+	$newVersion	= $nugetVersion.Split("-") | Select-Object -first 1
+	$message    = "NuGet version changed from '$nugetVersion' to '$newVersion'"
+
+	Add-AppveyorMessage $message
+	Write-Host $message
+
+	$nugetVersion = $newVersion
+}
+
+NuGet pack -Version $nugetVersion
 Push-AppveyorArtifact *.nupkg
