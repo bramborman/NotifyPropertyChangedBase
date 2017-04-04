0c8f4bd21f385905aa3f7de20bbcd762ab0edabc
Marian Dolinský <mariandolinsky@outlook.com>
2017-04-04 08:47:08 +0200

    Added a directive to skip deployment - testing it now [skip deployment]

--- AfterBuild.ps1
+++ AfterBuild.ps1
@@ -41,3 +41,19 @@ if($LastExitCode -ne 0)
 }
 
 Push-AppveyorArtifact *.nupkg
+
+# Deployment skipping
+$skipDeploymentDirective = "[skip deployment]"
+
+if (($env:APPVEYOR_REPO_COMMIT_MESSAGE -match $skipDeploymentDirective) -or ($env:APPVEYOR_REPO_COMMIT_MESSAGE_EXTENDED -match $skipDeploymentDirective))
+{
+	$message = "Commit message contains $skipDeploymentDirective so deployment is skipped in this build."
+	Add-AppveyorMessage $message
+	Write-Host $message
+
+	Set-AppveyorBuildVariable "SKIP_DEPLOYMENT" $true
+}
+else
+{
+	Set-AppveyorBuildVariable "SKIP_DEPLOYMENT" $false
+}
