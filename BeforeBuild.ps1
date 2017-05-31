83fad476ac70e2151cb233bbcd6dff259da01050
Marian Dolinský <mariandolinsky@outlook.com>
2017-05-31 20:45:36 +0200

    Added some weird scripting to patch .NET Core and .NET Standard - closes #13

--- BeforeBuild.ps1
+++ BeforeBuild.ps1
@@ -8,3 +8,19 @@ if($LastExitCode -ne 0)
 {
 	$host.SetShouldExit($LastExitCode)
 }
+
+# Patch .NET Core and .NET Standard
+Install-Module -Name powershell-yaml
+
+$yaml           = Get-Content .\appveyor.yml -Raw
+$appveyorConfig = ConvertFrom-Yaml $yaml
+$buildVersion   = $appveyorConfig.assembly_info.assembly_version.Replace("{build}", $env:APPVEYOR_BUILD_NUMBER)
+$csprojs        = "NotifyPropertyChangedBase.NetCore", "NotifyPropertyChangedBase.NetStandard"
+
+foreach ($csproj in $csprojs)
+{
+    $xml                    = [xml](Get-Content ".\$csproj\$csproj.csproj")
+    $propertyGroup          = $xml.Project.PropertyGroup
+    $propertyGroup.Version  = $buildVersion
+    $xml.Save($xmlPath)
+}
