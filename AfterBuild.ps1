513901b1b43a8d2548a237fd54c39c5946347c3d
Marian Dolinský <mariandolinsky@outlook.com>
2017-07-06 18:13:39 +0200

    More testing..

--- AfterBuild.ps1
+++ AfterBuild.ps1
@@ -42,4 +42,4 @@ Start-FileDownload "https://raw.githubusercontent.com/bramborman/AppVeyorBuildSc
 Start-FileDownload "https://raw.githubusercontent.com/bramborman/AppVeyorBuildScripts/master/Scripts/Deployment-Skipping.ps1"
 .\Deployment-Skipping.ps1
 
-dotnet vstest NotifyPropertyChangedBase.Tests.NetCore.dll /logger:Appveyor
+dotnet vstest NotifyPropertyChangedBase.Tests.NetCore.dll /logger:"C:\Program Files\AppVeyor\BuildAgent\Appveyor.MSBuildLogger.dll"
