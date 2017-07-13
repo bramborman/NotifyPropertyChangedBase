009b1d84e6a2dc3eaac52e416e084823e4d3796f
Marian Dolinský <mariandolinsky@outlook.com>
2017-07-13 22:05:44 +0200

    It'll maybe show the .NET Core test results in AppVeyor xD [skip deployment]

--- AppVeyor-AfterBuild.ps1
+++ AppVeyor-AfterBuild.ps1
@@ -42,30 +42,8 @@ Start-FileDownload "https://raw.githubusercontent.com/bramborman/AppVeyorBuildSc
 Start-FileDownload "https://raw.githubusercontent.com/bramborman/AppVeyorBuildScripts/master/Scripts/Deployment-Skipping.ps1"
 .\Deployment-Skipping.ps1
 
-dotnet vstest NotifyPropertyChangedBase.Tests.NetCore\bin\Release\netcoreapp1.0\NotifyPropertyChangedBase.Tests.NetCore.dll | Tee-Object -Variable testOutput
-$results = $testOutput | Where-Object { $_.StartsWith("Total tests:") }
-$counts = [Regex]::Matches($results, "\d+") | ForEach-Object { $_.Value }
-
-if ($counts.Count -ne 4)
-{
-    throw "Total number of numbers parsed is wrong. `$counts.Count: '$($counts.Count)'"
-}
-
-$total	 = [int]$counts[0]
-$passed  = [int]$counts[1]
-$failed  = [int]$counts[2]
-$skipped = [int]$counts[3]
-
-# Simple check whether we've parsed the results correctly
-if ($total -ne ($passed + $failed + $skipped))
-{
-    throw "Total number of test run was not the same as sum of test passed, failed and skipped."
-}
-
-if ($failed -ne 0)
-{
-    throw "$failed of unit tests failed"
-}
+dotnet vstest NotifyPropertyChangedBase.Tests.NetCore\bin\Release\netcoreapp1.0\NotifyPropertyChangedBase.Tests.NetCore.dll /logger:trx
+(New-Object "System.Net.WebClient").UploadFile("https://ci.appveyor.com/api/testresults/mstest/$env:APPVEYOR_JOB_ID", (Resolve-Path "NotifyPropertyChangedBase.Tests.NetCore\bin\Release\netcoreapp1.0\TestResults\*.trx"))
 
 $target = """C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\IDE\MSTest.exe"""
 $targetArgs = "/testcontainer:""NotifyPropertyChangedBase.Tests.Net45\bin\Release\NotifyPropertyChangedBase.Tests.Net45.dll"""
