a0649ac011e06e2ed4c69506716e796fd0dc8246
Marian Dolinský <mariandolinsky@outlook.com>
2017-09-05 18:29:28 +0200

    Kinda refactored Unit tests or what

--- NotifyPropertyChangedBase.Tests/Tests.cs
+++ NotifyPropertyChangedBase.Tests/Tests.cs
@@ -32,16 +32,17 @@ namespace NotifyPropertyChangedBase.Tests
     [TestClass]
     public sealed class Tests
     {
-        //U = uint
-        //L = long
-        //UL = ulong
-        //F = float
-        //M = decimal
-        //D = double
-
+        // U = uint
+        // L = long
+        // UL = ulong
+        // F = float
+        // M = decimal
+        // D = double
         private readonly string[] invalidPropertyNames = new string[] { null, "", "\n\t   \v\r" };
         private readonly object[] invalidInt32Values = new object[] { null, true, 'x', "", (byte)0, (sbyte)0, 0U, 0L, 0UL, 0F, 0M, 0D, new Test() };
-        
+        private readonly object[] invalidNullableInt32Values = new object[] { true, 'x', "", (byte)0, (sbyte)0, 0U, 0L, 0UL, 0F, 0M, 0D, new Test() };
+        private readonly object[] invalidTestValues = new object[] { true, 'x', "", (byte)0, (sbyte)0, 0, 0U, 0L, 0UL, 0F, 0M, 0D };
+
         [TestMethod]
         public void RegisterPropertyTests()
         {
@@ -55,24 +56,27 @@ namespace NotifyPropertyChangedBase.Tests
 
             // Invalid default value
             AllThrows<object, ArgumentException>(invalidInt32Values, invalidInt32Value => w.RegisterProperty("P", typeof(int), invalidInt32Value));
+            AllThrows<object, ArgumentException>(invalidNullableInt32Values, invalidNullableInt32Value => w.RegisterProperty("P", typeof(int?), invalidNullableInt32Value));
             Assert.ThrowsException<ArgumentException>(() => w.RegisterProperty("P", typeof(uint), -1));
+            AllThrows<object, ArgumentException>(invalidTestValues, invalidTestValue => w.RegisterProperty("P", typeof(ITest), invalidTestValue));
+            AllThrows<object, ArgumentException>(invalidTestValues, invalidTestValue => w.RegisterProperty("P", typeof(TestBase), invalidTestValue));
+            AllThrows<object, ArgumentException>(invalidTestValues, invalidTestValue => w.RegisterProperty("P", typeof(Test), invalidTestValue));
 
-            // Valid arguments
-            const string PROP_1 = "1";
-            w.RegisterProperty(PROP_1, typeof(int), 0);
-
-            // Registering another property with the same name
-            Assert.ThrowsException<ArgumentException>(() => w.RegisterProperty(PROP_1, typeof(int), 0));
+            const string PROP_INT32 = "Int32";
 
             // Valid default values
-            w.RegisterProperty("Nullable1", typeof(bool?), null);
-            w.RegisterProperty("Nullable2", typeof(bool?), true);
+            w.RegisterProperty(PROP_INT32, typeof(int), 0);
+            w.RegisterProperty("Nullable1", typeof(int?), null);
+            w.RegisterProperty("Nullable2", typeof(int?), 0);
             w.RegisterProperty("ITest", typeof(ITest), null);
             w.RegisterProperty("ITest2", typeof(ITest), new Test());
             w.RegisterProperty("TestBase", typeof(TestBase), null);
             w.RegisterProperty("TestBase2", typeof(TestBase), new Test());
             w.RegisterProperty("Test", typeof(Test), null);
             w.RegisterProperty("Test2", typeof(Test), new Test());
+
+            // Registering another property with the same name
+            Assert.ThrowsException<ArgumentException>(() => w.RegisterProperty(PROP_INT32, typeof(int), 0));
         }
 
         [TestMethod]
@@ -82,45 +86,46 @@ namespace NotifyPropertyChangedBase.Tests
 
             // Invalid property name
             AllThrows<string, ArgumentException>(invalidPropertyNames, invalidPropertyName => w.GetValue(invalidPropertyName));
+            AllThrows<string, ArgumentException>(invalidPropertyNames, invalidPropertyName => w.SetValue(invalidPropertyName));
+            AllThrows<string, ArgumentException>(invalidPropertyNames, invalidPropertyName => w.ForceSetValue(invalidPropertyName));
 
+            // Not registered property
+            Assert.ThrowsException<ArgumentException>(() => w.GetValue("1"));
+            Assert.ThrowsException<ArgumentException>(() => w.SetValue("1"));
+            Assert.ThrowsException<ArgumentException>(() => w.ForceSetValue("1"));
+
+            // Actual Get/Set/ForceSetValue tests
+            Test<int>("Int32", typeof(int), value => value + 1, invalidInt32Values);
+            Test<int?>("NullableInt32", typeof(int?), value => value == null ? 0 : value.Value + 1, invalidNullableInt32Values);
+            Test<Test>("ITest", typeof(ITest), value => new Test(), invalidTestValues);
+            Test<Test>("TestBase", typeof(TestBase), value => new Test(), invalidTestValues);
+            Test<Test>("Test", typeof(Test), value => new Test(), invalidTestValues);
+
+            void Test<TValue>(string propertyName, Type propertyType, Func<TValue, TValue> getNewValue, IEnumerable<object> invalidValues)
             {
-                const string PROP_1 = "1";
+                TValue value = getNewValue(default(TValue));
+                w.RegisterProperty(propertyName, propertyType, value);
+                Assert.AreEqual(value, w.GetValue(propertyName));
 
-                int prop1Value = 0;
-                w.RegisterProperty(PROP_1, typeof(int), prop1Value);
-                Assert.AreEqual(prop1Value, w.GetValue(PROP_1));
+                value = getNewValue(value);
+                Assert.AreNotEqual(value, w.GetValue(propertyName));
 
-                prop1Value++;
-                Assert.AreNotEqual(prop1Value, w.GetValue(PROP_1));
+                w.SetValue(value, propertyName);
+                Assert.AreEqual(value, w.GetValue(propertyName));
 
-                w.SetValue(prop1Value, PROP_1);
-                Assert.AreEqual(prop1Value, w.GetValue(PROP_1));
+                w.SetValue(default(TValue), propertyName);
+                Assert.AreEqual(default(TValue), w.GetValue(propertyName));
 
-                prop1Value++;
-                w.ForceSetValue(prop1Value, PROP_1);
-                Assert.AreEqual(prop1Value, w.GetValue(PROP_1));
+                value = getNewValue(value);
+                w.ForceSetValue(value, propertyName);
+                Assert.AreEqual(value, w.GetValue(propertyName));
+                
+                w.ForceSetValue(default(TValue), propertyName);
+                Assert.AreEqual(default(TValue), w.GetValue(propertyName));
 
                 // Invalid value
-                AllThrows<object, ArgumentException>(invalidInt32Values, invalidInt32Value => w.SetValue(invalidInt32Value, PROP_1));
-                AllThrows<object, ArgumentException>(invalidInt32Values, invalidInt32Value => w.ForceSetValue(invalidInt32Value, PROP_1));
-            }
-
-            {
-                const string PROP_2 = "2";
-
-                Test prop2Value = new Test();
-                w.RegisterProperty(PROP_2, typeof(Test), prop2Value);
-                Assert.AreEqual(prop2Value, w.GetValue(PROP_2));
-
-                prop2Value = new Test();
-                Assert.AreNotEqual(prop2Value, w.GetValue(PROP_2));
-
-                w.SetValue(prop2Value, PROP_2);
-                Assert.AreEqual(prop2Value, w.GetValue(PROP_2));
-
-                prop2Value = new Test();
-                w.ForceSetValue(prop2Value, PROP_2);
-                Assert.AreEqual(prop2Value, w.GetValue(PROP_2));
+                AllThrows<object, ArgumentException>(invalidValues, invalidValue => w.SetValue(invalidValue, propertyName));
+                AllThrows<object, ArgumentException>(invalidValues, invalidValue => w.ForceSetValue(invalidValue, propertyName));
             }
         }
 
