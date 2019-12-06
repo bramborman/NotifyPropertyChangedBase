fac80807881d756486eebd8d5fb160c46ea693a6
Marian Dolinský <mariandolinsky@outlook.com>
2019-12-06 21:16:27 +0100

    This will work! I'M SURE

--- AppVeyor-Init.ps1
+++ AppVeyor-Init.ps1
@@ -33,8 +33,8 @@ Start-FileDownload "https://raw.githubusercontent.com/bramborman/AppVeyorBuildSc
 Start-FileDownload "https://raw.githubusercontent.com/bramborman/AppVeyorBuildScripts/master/Scripts/Set-PureBuildVersion.ps1"
 .\Set-PureBuildVersion.ps1
 
-Start-FileDownload "https://raw.githubusercontent.com/bramborman/AppVeyorBuildScripts/master/Scripts/Set-SemanticVersionVariable.ps1"
-.\Set-SemanticVersionVariable.ps1
+Start-FileDownload "https://raw.githubusercontent.com/bramborman/AppVeyorBuildScripts/master/Scripts/Set-MajorMinorPatchBuildVersionVariable.ps1"
+.\Set-MajorMinorPatchBuildVersionVariable.ps1
 
 Start-FileDownload "https://raw.githubusercontent.com/bramborman/AppVeyorBuildScripts/master/Scripts/Deployment-Skipping.ps1"
 .\Deployment-Skipping.ps1
